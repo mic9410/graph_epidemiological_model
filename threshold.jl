c0e128e59b307a4f122d94c372eb026dc2eec6ef
mic9410 <michal.foryt@gmail.com>
2020-01-19 23:18:51 +0100

    corrected  method

--- threshold.jl
+++ threshold.jl
@@ -17,7 +17,7 @@ dist = Normal(0.5,0.25); #mi - średnia, sigma - odchylenie standardowe, a więc
 n_nodes = 10;
 n_edges = 7;
 rew_prob = 0.15; # prawdopodobieństwo zarażenia
-per_infected = 0.2; # Liczba wolontariuszy (zarażonych) na osobę
+per_infected = 0.45; # Liczba zarażonych na osobę
 infection_duration = 10; # Czas trwania infekcji
 per_vaccinated = 0.2; # Liczba osób zaszczepionych
 ########################
@@ -28,7 +28,7 @@ function generate_social_network(n_nodes, n_edges, rew_prob, dist)
     for i = 1:n_nodes # Inicjalizujemy wartości wszystkich osób
 		#dodajemy do niego parametry opisujace agentow. Ich wewnetrzna motywacje do dzialania
 		#(w SIR bedzie to wewnętrzna odporność), wskaznik tego czy sa aktualnie aktywni (zarażeni), odporni na infekcję, ile dni zarażony
-		set_props!(graph,i,Dict(:exposed_to_infection=>rand(dist), :active => 0, :immune => 0, :inf_days => 0))
+		set_props!(graph,i,Dict(:exposed_to_infection=>rand(dist), :infected => 0, :immune => 0, :inf_days => 0))
     end
     return graph
 end
@@ -40,30 +40,29 @@ function initialize(n_nodes, n_edges, rew_prob, dist, per_infected)
 	network = generate_social_network(n_nodes, n_edges, rew_prob, dist) #wywolujemy funkcje tworzaca graf
 	for i = 1:n_nodes #sprawdzamy czy nie istnieja agenci aktywni od poczatku
 		if get_prop(network ,i, :exposed_to_infection)  >= 1 # funkcja get_prop wyciaga odpowedni argument z wierzcholka
-			set_prop!(network ,i, :active, 1) #set_prop zmienia lub dodaje nowy parametr do wierzcholka
+			set_prop!(network ,i, :infected, 1) #set_prop zmienia lub dodaje nowy parametr do wierzcholka
         end
     end
     infected = rand(1:n_nodes,Int(round(per_infected*n_nodes))) #losowo wybieramy agentow, ktorzy beda aktywni od poczatku
     for j in infected
-        set_prop!(network ,j, :active, 1) # Dla wylosowanych agentów ustawiamy parametr active na true
+        set_prop!(network ,j, :infected, 1) # Dla wylosowanych agentów ustawiamy parametr active na true
 	end
 
 	immune_candidates = Int[]
 	for j in 1:n_nodes
-		if get_prop(network ,j, :active) == 0	# osoba nie może być jednocześnie chora i odporna, dlatego należy odfiltrować osoby chore
+		if get_prop(network ,j, :infected) == 0	# osoba nie może być jednocześnie chora i odporna, dlatego należy odfiltrować osoby chore
 			push!(immune_candidates, j) # numery wierzchołków - kandydatów do szczepienia zapisujemy w tabeli immune_candidates
 		end
 	end
 
-	immuned = rand(1:length(immune_candidates),Int(round(per_vaccinated*length(immune_candidates)))) #losowo wybieramy agentow, ktorzy beda aktywni od poczatku
+	#losowo wybieramy agentow, ktorzy beda zarażeni od poczatku
+	immuned = rand(1:length(immune_candidates),Int(round(per_vaccinated*length(immune_candidates))))
+
 	# Spośród kandydatów do szczepienia wybieramy zbiór osób które zostaną zaszczepione
 	for j in 1:length(immuned)
-		set_prop!(network ,immuned[j], :immune, 1)
+		set_prop!(network ,immune_candidates[immuned[j]], :immune, 1)
 	end
 
-	for j in 1:n_nodes
-		println(j, " - immuned: ", get_prop(network ,j, :immune),", infected: ", get_prop(network ,j, :active))
-	end
 	return network # Zwracamy zainicjalizowaną siec
 end
 
@@ -74,10 +73,10 @@ end
 function is_active(graph)
     final_event = 0 #zmienna pomocnicza sterujaca symulacja. Gdy ma wartosc 0 to wiemy, ze w symulacji nic juz sie nie zmienia i nie ma koniecznosci jej dalej kontynuowac
     for i = 1:nv(graph)
-        if get_prop(graph ,i, :active) == 0
-           external_motivation =  - (1 - sum(get_prop(graph ,j, :active)  for j in neighbors(graph,i)) / length(neighbors(graph,i))) #wyliczamy zewnetrzna motywacje do dzialania. Przyjmuje ona wartosc od -1 do 0 i rosnie gdy sasiedzi agenta staja sie aktywni
+        if get_prop(graph ,i, :infected) == 0
+           external_motivation =  - (1 - sum(get_prop(graph ,j, :infected)  for j in neighbors(graph,i)) / length(neighbors(graph,i))) #wyliczamy zewnetrzna motywacje do dzialania. Przyjmuje ona wartosc od -1 do 0 i rosnie gdy sasiedzi agenta staja sie aktywni
            if get_prop(graph ,i, :exposed_to_infection) + external_motivation > 0 #sprawdzamy czy suma motywacji agenta jest wieksza od 0
-                set_prop!(graph ,i,:active, 1) #jesli tak to staje sie on aktywny
+                set_prop!(graph ,i,:infected, 1) #jesli tak to staje sie on aktywny
                 final_event = 1 #a symulacja trwa dalej
             end
         end
@@ -89,16 +88,16 @@ end
 # funkcja sterujaca symulacja
 function run_simulation(n_nodes, n_edges, rew_prob, dist, per_infected, infection_duration, per_vaccinated, max_iter = 1, plotting = true)
 	network = initialize(n_nodes, n_edges, rew_prob, dist, per_infected) #tworzymy siec
-	active_beginning = sum(get_prop(network ,j, :active)  for j = 1: nv(network)) # bierzemy sumę aktywnych agentów
+	active_beginning = sum(get_prop(network ,j, :infected)  for j = 1: nv(network)) # bierzemy sumę aktywnych agentów
     for i = 1:max_iter #zaczynamy symulacje
         done = is_active(network)
         if done == 1 #jezeli zmienna final_event jest rowna 0 oznacza to, ze w symulacji nic juz sie nie dzieje - nie ma sensu jej kontynuowac
 			break
         end
     end
-    active_end = sum(get_prop(network ,j, :active)  for j = 1:nv(network))
+    active_end = sum(get_prop(network ,j, :infected)  for j = 1:nv(network))
 	if plotting
-		membership = [get_prop(network ,j, :active)  for j = 1:nv(network)] .+ 1 #tworzymy wektor 1 i 2 oznaczajacy nieaktywnych (1) i aktywnych (2) agentow
+		membership = [get_prop(network ,j, :infected)  for j = 1:nv(network)] .+ 1 #tworzymy wektor 1 i 2 oznaczajacy nieaktywnych (1) i aktywnych (2) agentow
 		nodecolor = [colorant"lightseagreen", colorant"magenta"] #dodajemy kolory lightseagreen dla nieaktywnych i magenta dla aktywnych
 		nodefillc = nodecolor[membership] #kojarzymy kolory z wezlami grafu
 		#tworzymy rysunek:
